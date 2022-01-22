a67dc135ac108bc97066e4dcb3880aee96c3e40d
Ejaz Ahmad <ejaz.ahmad@schuja.com>
2022-01-23 00:02:05 +0500

    up

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, Pressable} from 'react-native';
 
 class Home extends React.Component {
   render() {
     return (
-      <View>
+      <View style={{flex: 1}}>
         <Text>This is Home screen</Text>
+        <Button title="Click me again" onPress={() => alert("Hello")} />
+        <Pressable onPress={() => Alert.alert('Simple Button pressed')}>
+            <Text>I'm pressable!</Text>
+            </Pressable>
       </View>
     );
   }
