52d19dc6a283a7085c89d35c0aafc74aa95b2cdd
Ejaz Ahmad <ejaz.ahmad@schuja.com>
2022-01-27 18:38:41 +0500

    up

--- src/Screens/Splash/Splash1.js
+++ src/Screens/Splash/Splash1.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 
 class Splash1 extends Component {
     render(){
         setTimeout(() =>{
             this.props.navigation.navigate('Home')
-        }, 3000)
+        }, 5000)
         return (
             <View>
                 <Text style={{fontSize:42}}>Splash</Text>
+                <Button 
+                    title="Skip Intro"
+          onPress={() =>
+            this.props.navigation.navigate('Home')
+          }
+        />
             </View>
         )
     }
