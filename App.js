fce6d53a4f03ffcd5171c0ab53397eae6adfaacf
Ejaz Ahmad <ejaz.ahmad@schuja.com>
2022-01-27 11:00:11 +0500

    up

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { Component }  from 'react';
 import  { View, Text, ScrollView } from 'react-native';
 import Routes from './src/Navigation/Routes';
-import TestScreen from './src/redux'
-// import TestScreen from './src/Screens/Profile/Profile';
+// import TestScreen from './src/redux'
+import TestScreen from './src/Screens/Profile/Profile';
 class App extends Component {
   render(){
     return (
