018f3b7fe25b525efc7689ab599e1736cfbe5c1f
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2022-02-25 11:33:46 +0500

    downgrade version

--- App.js
+++ App.js
@@ -1,16 +1,59 @@
-import React, { Component }  from 'react';
-import  { View, Text, SafeAreaView, StatusBar }from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-class App extends Component {
-  render(){
-    return(
-      <SafeAreaView>
-      <View>
-        <Text>This is test screen 111</Text>
-      </View>
-      </SafeAreaView>
-    )
-  }
-}
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+
+  return (
+    <SafeAreaView>
+        <View>
+            <Text>Hello</Text>
+        </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
 export default App;
