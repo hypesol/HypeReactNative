bb751c995268d1a14bb24f041be791243fac4ee4
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2022-03-08 16:01:00 +0500

    saga

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -8,17 +8,26 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import rootReducer from '../reducers/index'; // Call rootReducer which have all reducers
 
+import createSagaMiddleware from '@redux-saga/core';
+
+// import sagass from './sagas';
+
 // PERSIST Configtations
-const persistConfig = {
+  const persistConfig = {
     key: 'root',
     storage: AsyncStorage, // we can use default storage like above or we can use AsyncStorage also
     // stateReconciler: hardSet,
     // blacklist :['',''] // Do not save specific reducer
   }
 
+  const middleWare = [];
+  const sagaMiddleWare = createSagaMiddleware();
+
+  middleWare.push(sagaMiddleWare);
+
   const persistedReducer = persistReducer(persistConfig, rootReducer)
   
-   const store = createStore(persistedReducer);
+   const store = createStore(persistedReducer, applyMiddleware(sagaMiddleWare));
    let persistor = persistStore(store);
    
    export { store, persistor}
