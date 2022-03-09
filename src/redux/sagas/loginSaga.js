7db354d30c841c91752513bfd0ddb966b6c06902
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2022-03-09 17:13:49 +0500

    API

--- src/redux/sagas/loginSaga.js
+++ src/redux/sagas/loginSaga.js
@@ -1,32 +1,49 @@
 import { call } from "redux-saga/effects";
 
 export default function* loginInfo(action){
-    console.log(action.email);
+    // export const loginInfo= (action) => {
+    console.log(action);
     // const apiURL = "https://dev-accounts.seebiz.com/getAdOnStatus?adOnKey=seebiz";
     const apiURL = "https://dev-accounts.seebiz.com/login";
     getLoginInfo = async data =>{
-        console.log(data["email"]);
-        const response = await fetch(apiURL,
-             {
-               method: 'POST',
-               headers: {
-                // 'API-key': apikey,
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-               },
-               body: JSON.stringify(data) 
-             }
-            );
-            console.log("Hi");
-            console.log(response);
-            //return response.json();
+        // console.log("Login Info: ",data.data);
+        const dataObj= JSON.stringify({ "email": "test@gmail.com", "passsword":"123"});
+        const formData = JSON.stringify(data.data);
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: formData,
+                redirect: 'follow'
+              };
+
+         const userInfo = await fetch("https://dev-accounts.seebiz.com/login/", requestOptions)
+            .then(response => response.text())
+            .then(response => { 
+                // console.log(response);
+                // return response.json();
+                return response;
+            })
+            .then(result => {
+                return result;
+            })
+            .catch(error => {
+                // console.log('error', error);
+                return error;
+            });
+            
+            return userInfo;
     }
 
     try{
-        yield call(getLoginInfo, action)
+        const userData = yield call(getLoginInfo, action);
+        console.log("TEST:- ",userData);
     }
     catch(error){
-        console.log("Error");
+        console.log("Error",error);
     }
 
 }
