0794341d72ebdddae4aa2d6777888e8ab0e7162b
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2022-03-10 12:00:24 +0500

    login

---
+++ src/Screens/test.js
@@ -0,0 +1,44 @@
+export const TestData = [{
+    "code": 200,
+    "serviceUrl": "https://dev-accounts.seebiz.com",
+    "ssoToken": "No",
+    "success": true,
+    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7InVzZXJfaWQiOiI2MWU2YzExNWQxYjM1OWIwYjJjNDk3OTEifSwiZW1haWwiOiJ0ZXN0aGFqYWpAZ21haWwuY29tIiwic3ViIjoiNjFlNmMxMTVkMWIzNTliMGIyYzQ5NzkxIiwiZXhwaXJlIjoxNjQ2ODkyNzI1NzY5fQ.hCNuQoVtsxkOM3zbkhZuG978Hil_MdnpmmT6Aw7ml0k",
+    "user": {
+        "__v": 0,
+        "_id": "61e6c115d1b359b0b2c49791",
+        "created_at": "2022-01-18T13:31:01.585Z",
+        "deletedBy": null, 
+        "deleted_at": null, 
+        "email": "testhajaj@gmail.com", 
+        "emailChange": { 
+            "email": null, 
+            "expired": "2022-01-26T07:46:57.734Z", 
+            "securityCode": null 
+        }, 
+        "email_verification": true, 
+        "firstName": "Haroon", 
+        "intermediateToken": "", 
+        "isTrashed": false, 
+        "lastName": "SQA", 
+        "origin": "seebiz", 
+        "originCreationData": 
+        "2020-07-09T06:39:59.533Z", 
+        "originInfo": [[Object]], 
+        "phone": "1111111111", 
+        "phone_number_verification": false, 
+        "profilePic": "https://s3-us-west-2.amazonaws.com/seebiz-development/5f06bbbfa8a2a641799417e3/profile/original/1642485011944-0SE.jpeg", 
+        "ssoStatus": true, 
+        "status": { 
+            "_id": "61f0fc719ab34d67df7ecda0", 
+            "active": true, 
+            "suspended": false 
+        }, 
+        "updated_at": "2022-03-09T11:27:25.268Z", 
+        "username": "hajaj test", 
+        "usernameChange": { 
+            "changeDate": null, 
+            "username": null 
+        }
+    }
+}]
