4ad83238c02451136903db8da313f12901fffbd3
Ejaz Ahmad <ejaz.dev@pk.see.biz>
2022-04-19 18:19:00 +0500

    wip: new screens

---
+++ src/consts/media.js
@@ -0,0 +1,13 @@
+const media = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: '99.99',
+    like: true,
+    img:require('../assets/images/avatar-1.jpg'),
+    about: 'This is about of this product',
+  },
+  
+]
+
+export default media;
